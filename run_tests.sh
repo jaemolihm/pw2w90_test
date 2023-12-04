b9a6d8940c2baf3569f0393dc2691b8d40113199
Jae-Mo Lihm <jaemolim96@gmail.com>
2023-12-04 10:04:00 +0900

    Add another atom_proj test. With external projectors

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ set -e
 # lib_diam is not tested by default.
 
 if [ $# -eq 0 ]; then
-  args="si si_scdm_erfc si_scdm_gaussian si_scdm_isolated benzene o2 pt gaas gaas_nosoc col_fe_up col_fe_dn col_fe_paw_up col_fe_paw_dn fe mn3ir Cu_irrbz Fe_irrbz gaas_atproj unk_si unk_gaas unk_col_fe"
+  args="si si_scdm_erfc si_scdm_gaussian si_scdm_isolated benzene o2 pt gaas gaas_nosoc col_fe_up col_fe_dn col_fe_paw_up col_fe_paw_dn fe mn3ir Cu_irrbz Fe_irrbz gaas_atproj si_atproj unk_si unk_gaas unk_col_fe"
 elif [ $1 == "scdm" ]; then
   args="si_scdm_erfc si_scdm_gaussian si_scdm_isolated pt mn3ir"
 elif [ $1 == "unk" ]; then
@@ -12,7 +12,7 @@ elif [ $1 == "unk" ]; then
 elif [ $1 == "irrbz" ]; then
   args="Cu_irrbz Fe_irrbz"
 elif [ $1 == "atproj" ]; then
-  args="gaas_atproj"
+  args="gaas_atproj si_atproj"
 elif [ $1 == "lib" ]; then
   args="lib_diam"
 else
---
+++ si_atproj/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+PREFIX="si"
+
+. ../environment_variables
+
+if [ $GENERATE_REFERENCE == "true" ]; then
+    echo "Regenerate reference data for $PREFIX"
+    mkdir -p reference
+    # SCF, NSCF
+    $RUN_PARA_PREFIX $QE_REF/pw.x $RUN_PARA_SUFFIX_POOL -in scf.in > scf.out
+    $RUN_PARA_PREFIX $QE_REF/pw.x $RUN_PARA_SUFFIX_POOL -in nscf.in > nscf.out
+    $RUN_SERIAL_PREFIX $WANN -pp $PREFIX
+    # pw2wannier90 with reference program
+    $RUN_PARA_PREFIX $QE_REF/pw2wannier90.x $RUN_PARA_SUFFIX_NO_POOL -in pw2wan.in > reference/pw2wan.out
+    cp $PREFIX.* reference/
+fi
+
+for tag in amn mmn eig spn uHu uIu sHu sIu unkg; do
+    rm -f $PREFIX*.$tag*
+done
+
+echo "Test $PREFIX without pools"
+$RUN_PREFIX $QE_TEST/pw2wannier90.x $RUN_SUFFIX_NO_POOL -in pw2wan.in > pw2wan.out
+./test.py
+
+for tag in amn mmn eig spn uHu uIu sHu sIu unkg; do
+    rm -f $PREFIX*.$tag*
+done
+
+echo "Test $PREFIX with pools"
+$RUN_PREFIX $QE_TEST/pw2wannier90.x $RUN_SUFFIX_POOL -in pw2wan.in > pw2wan.pool.out
+./test.py
+
+for tag in amn mmn eig spn uHu uIu sHu sIu unkg; do
+    rm -f $PREFIX*.$tag*
+done
+
+if [ ! -z "$QE_TEST_SERIAL" ]; then
+    echo "Test $PREFIX with serial compilation"
+    $RUN_SERIAL_PREFIX $QE_TEST_SERIAL/pw2wannier90.x $RUN_SERIAL_SUFFIX -in pw2wan.in > pw2wan.out
+    ./test.py
+fi
