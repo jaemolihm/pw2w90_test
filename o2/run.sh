0bb3046c674a67855d9d69ba8de74eb67f6b99ae
Jae-Mo Lihm <jaemolim96@gmail.com>
2023-12-04 09:06:05 +0900

    Run SCF and NSCF of Gamma-only calculations without pools

--- o2/run.sh
+++ o2/run.sh
@@ -9,8 +9,8 @@ if [ $GENERATE_REFERENCE == "true" ]; then
     echo "Regenerate reference data for $PREFIX"
     mkdir -p reference
     # SCF, NSCF
-    $RUN_PARA_PREFIX $QE_REF/pw.x $RUN_PARA_SUFFIX_POOL -in scf.in > scf.out
-    $RUN_PARA_PREFIX $QE_REF/pw.x $RUN_PARA_SUFFIX_POOL -in nscf.in > nscf.out
+    $RUN_PARA_PREFIX $QE_REF/pw.x $RUN_PARA_SUFFIX_NO_POOL -in scf.in > scf.out
+    $RUN_PARA_PREFIX $QE_REF/pw.x $RUN_PARA_SUFFIX_NO_POOL -in nscf.in > nscf.out
     $RUN_SERIAL_PREFIX $WANN -pp $PREFIX
     # pw2wannier90 with reference program
     for spin in up dn; do
